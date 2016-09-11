e61eb52566472095a39144eb0a6894fd5d72203c
nirasan <nirasan@gmail.com>
2016-09-12 07:35:21 +0900

    add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Go JSON Web Token Handler
+This is a http.Handler for authentication and authorization using JWT (JSON Web Token).
+
+## Dependences
+* Go 1.7 or higher
+* github.com/dgrijalva/jwt-go
+
+## Install
+```sh
+go get github.com/dgrijalva/jwt-go
+go get github.com/nirasan/go-jwt-handler
+```
+
+## Example
--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package JSON Web Token Authentication and Authorization Handler.
+// JSON Web Token Authentication and Authorization Handler.
 // It uses "github.com/dgrijalva/jwt-go" to provide a jwt authentication.
 // This package requires go1.7 or higher because it uses http.Request.Context to the delivery of the token.
 package jwthandler
